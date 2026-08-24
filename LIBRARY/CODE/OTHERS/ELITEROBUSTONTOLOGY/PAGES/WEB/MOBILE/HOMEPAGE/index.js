ebb1c2564b5af3e54aa84a958653d14242589e81
deuelion <316406166+deuelion@users.noreply.github.com>
2026-08-24 08:26:54 +0300

    Updates

--- LIBRARY/CODE/OTHERS/ELITEROBUSTONTOLOGY/PAGES/WEB/MOBILE/HOMEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/ELITEROBUSTONTOLOGY/PAGES/WEB/MOBILE/HOMEPAGE/index.js
@@ -1,19 +1,17 @@
-import { LEFTICON } from "../../../../../../../CONNECTION";
-
 export const ELITEROBUSTONTOLOGYWEBMOBILEPAGE=()=>{
     HEADERVIEWSPAGE((HOLDER)=>{
-        LEFTICON(HOLDER,)
-        LEFTTEXT(HOLDER,"Elite Robust Ontology",()=>{
+        LEFTICON(HOLDER,ELITEROBUSTONTOLOGYLOGO,ELITEROBUSTONTOLOGYLOGO,()=>{
+
+        });
+        HEADER(HOLDER,"Elite Robust Ontology",()=>{
 
         });
         RIGHTICON(HOLDER,WHITEMENUICON,BLACKMENUICON,()=>{
 
         });
     },(HOLDER)=>{
-
         HEADER(HOLDER,"WELCOME TO ELITE ROBUST ONTOLOGY",()=>{
 
         });
-
     });
 };
