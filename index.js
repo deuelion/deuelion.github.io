1d65c55633ed3e87c85a74e74221b9ca0fcc02bc
deuelion <316406166+deuelion@users.noreply.github.com>
2026-08-30 15:08:12 +0300

    Updates

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ localStorage.setItem("ExpiredMessage",HOSTEXPIREDMESSAGE);
 if (localStorage.getItem("Expired")) {
   DISPLAY("",HOSTEXPIREDMESSAGE);RUNNER();
 } else {
-  if (STATUS === "Active") {
+  if (STATUS === "Active" ||TITLE === "EliteBuilder") {
     RUNNER();
   }else{
     UPDATES();
