75b42592de71d83462a56e26ee01d4373bf2a4bc
deuelion <316406166+deuelion@users.noreply.github.com>
2026-08-30 14:39:38 +0300

    Updates

--- LIBRARY/CODE/OTHERS/MOVIELANDER/PAGES/ANDROID/ANDROID/LOGINPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MOVIELANDER/PAGES/ANDROID/ANDROID/LOGINPAGE/index.js
@@ -1,9 +1,24 @@
 export const MOVIELANDERANDROIDWEBLOGINPAGE=()=>{
-
-    DISPLAY("",`
-
-        <h1>LOGIN PAGE</h1>
-        
-    `);
-
+    IMAGEVIEW("",MOVIELANDERLOGO,(DATA)=>{
+            WIDTH(DATA,"90%");
+            HEIGHT(DATA,"30%");
+            BORDERRADIUS(DATA,"20px");
+    });
+    HEADER("","Your Home Cinema",(DATA)=>{
+        FONTSIZE(DATA,"25px");
+    });
+    BREAK("",()=>{});BREAK("",()=>{});
+    INPUT("","email","Enter Your Email","UserEmail",()=>{
+    });
+    BREAK("",()=>{});BREAK("",()=>{});
+    INPUT("","email","Enter Your Password","UserPassword",()=>{
+    });
+    BREAK("",()=>{});BREAK("",()=>{});
+    RIGHTTEXT("","Forgot Password?",(DATA)=>{
+        MARGINRIGHT(DATA,"8%");
+    });
+    BREAK("",()=>{});
+    BUTTON("","Sign In",(DATA)=>{
+        BACKGROUND(DATA,"forestgreen");
+    });
 };
