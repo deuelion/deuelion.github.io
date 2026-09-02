ece07520f6a851be908ee5922c3d3f2e801efea7
deuelion <316406166+deuelion@users.noreply.github.com>
2026-09-02 16:50:18 +0300

    Updates

--- LIBRARY/CODE/OTHERS/MOVIELANDER/PAGES/ANDROID/ANDROID/PROFILEPAGE/index.js
+++ LIBRARY/CODE/OTHERS/MOVIELANDER/PAGES/ANDROID/ANDROID/PROFILEPAGE/index.js
@@ -1,16 +1,75 @@
 export const MOVIELANDERANDROIDWEBPROFILEPAGE=()=>{
-    HEADERVIEWSPAGE((HOLDER)=>{
-        LEFTICON(HOLDER,WHITEBACKICON,BLACKBACKICON,(DATA)=>{
-            CLICK(DATA,()=>{
-                ROUTE("",MOVIELANDERANDROIDWEBPAGE,"MOVIELANDERANDROIDWEBPAGE");
+    GETINDEX("USERDATA","USERDATA",(Data)=>{
+        HEADERVIEWSPAGE((HOLDER)=>{
+            LEFTICON(HOLDER,WHITEBACKICON,BLACKBACKICON,(DATA)=>{
+                CLICK(DATA,()=>{
+                    ROUTE("",MOVIELANDERANDROIDWEBPAGE,"MOVIELANDERANDROIDWEBPAGE");
+                });
             });
-        });
-        RIGHTTEXT(HOLDER,"Profile",()=>{
+            RIGHTTEXT(HOLDER,"Profile",()=>{
 
-        });
-    },(HOLDER)=>{
-        HEADER(HOLDER,"PROFILE",()=>{
+            });
+        },(HOLDER)=>{
+            IMAGEVIEW(HOLDER,MOVIELANDERLOGO,(DATA)=>{
+                WIDTH(DATA,"90%");
+                HEIGHT(DATA,"40%");
+                BORDERRADIUS(DATA,"10px");
+            });
+            INLINEVIEW(HOLDER,(DATA)=>{
+                LEFTICON(DATA,WHITEUSERICON,BLACKUSERICON,(DATATA)=>{
+                    MARGINLEFT(DATATA,"10%");
+                });
+                RIGHTTEXT(DATA,"Name",(DATATA)=>{
+                    MARGINRIGHT(DATATA,"10%");
+                });
+            });
+            INLINEVIEW(HOLDER,(DATA)=>{
+                LEFTICON(DATA,WHITEGMAILICON,WHITEGMAILICON,(DATATA)=>{
+                    MARGINLEFT(DATATA,"10%");
+                });
+                RIGHTTEXT(DATA,"Email",(DATATA)=>{
+                    MARGINRIGHT(DATATA,"10%");
+                });
+            });
+            INLINEVIEW(HOLDER,(DATA)=>{
+                LEFTICON(DATA,WHITELOCATIONICON,BLACKLOCATIONICON,(DATATA)=>{
+                    MARGINLEFT(DATATA,"10%");
+                });
+                RIGHTTEXT(DATA,"Uganda",(DATATA)=>{
+                    MARGINRIGHT(DATATA,"10%");
+                });
+            });
+            HEADER(HOLDER,"PREFERENCES",(DATA)=>{
+                FONTSIZE(DATA,"25px");
+            });
+            BREAK(HOLDER,()=>{});
+            BUTTON(HOLDER,"Settings",(DATA)=>{
+                BACKGROUND(DATA,"teal");
+                CLICK(DATA,()=>{
+                    BOTTOMVIEW("App Settings",()=>{
 
+                    });
+                });
+            });
+            BREAK(HOLDER,()=>{});BREAK(HOLDER,()=>{});
+            BUTTON(HOLDER,"Policies",(DATA)=>{
+                BACKGROUND(DATA,"teal");
+                CLICK(DATA,()=>{
+                    BOTTOMVIEW("App Policies",()=>{
+
+                    });
+                });
+            });
+            BREAK(HOLDER,()=>{});BREAK(HOLDER,()=>{});
+            BUTTON(HOLDER,"Delete Account",(DATA)=>{
+                BACKGROUND(DATA,"red");
+                CLICK(DATA,()=>{
+                    BOTTOMVIEW("Delete Account",()=>{
+
+                    });
+                });
+            });
+            BREAK(HOLDER,()=>{});BREAK(HOLDER,()=>{});
         });
     });
 };
