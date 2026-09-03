36bc60469e059c6801e7ee04c638941e38b03ca9
deuelion <316406166+deuelion@users.noreply.github.com>
2026-09-03 14:42:10 +0300

    Updates

--- LIBRARY/SERVER/DATABASE/AUTH/index.js
+++ LIBRARY/SERVER/DATABASE/AUTH/index.js
@@ -1,8 +1,9 @@
 export const AUTH=()=>{
+    const MAIN=localStorage.getItem("API");
     OFFLINE(()=>{
         TOAST("","Please Wait,Verification In Progress!",()=>{            
             CONDITION(sessionStorage.getItem("Access") === "True",()=>{
-                GETDATA(MAINCONNECTIONAPI,"CONNECTION",(Data)=>{
+                GETDATA(MAIN,"User",(Data)=>{
                     FINDER(Data,"USEREMAIL",sessionStorage.getItem("UserEmail"),(User)=>{
                         TOASTEDMESSAGE(User.USEREMAIL === sessionStorage.getItem("UserEmail"),"Wrong User Email",()=>{
                             TOASTEDMESSAGE(User.USERPASSWORD === sessionStorage.getItem("UserPassword"),"Wrong User Password",()=>{
@@ -16,7 +17,7 @@ export const AUTH=()=>{
                 });
             },()=>{
                 CONDITION(sessionStorage.getItem("Access") === "False",()=>{
-                    GETDATA(MAINCONNECTIONAPI,"CONNECTION",(Data)=>{
+                    GETDATA(MAIN,"User",(Data)=>{
                         FINDER(Data,"USEREMAIL",sessionStorage.getItem("UserEmail"),(User)=>{
                             TOASTEDMESSAGE(User.USEREMAIL === sessionStorage.getItem("UserEmail"),"User Email Doesnot Exist",()=>{
                                 RELOAD();
@@ -24,17 +25,15 @@ export const AUTH=()=>{
                         });
                     });
                 },()=>{
-                    GETDATA(MAINCONNECTIONAPI,"CONNECTION",(Data)=>{
+                    GETDATA(MAIN,"User",(Data)=>{
                         REDUX(Data,(Element)=>{
                             CHECK( sessionStorage.getItem("UserEmail") !== Element.USEREMAIL,()=>{
                                 const HEADER=["FIRSTNAME", "MIDDLENAME", "LASTNAME", "USEREMAIL", "USERPASSWORD", "LOCATION", "IMAGE", "APPNAME", "STATUS", "ACCESS", "DATECREATED", "DATEUPDATED", "ACCOUNTS", "PHONENUMBER", "PREMIUM", "DEVICE", "COUNTRY"];
                                 const DATA=[sessionStorage.getItem("UserName"),"","",sessionStorage.getItem("UserEmail"),sessionStorage.getItem("UserPassword"),localStorage.getItem("TIMEZONE"),"",localStorage.getItem("AppName"),"","Approved",new Date(),"","","","","",""];
                                 CHECK(!localStorage.getItem("User"),()=>{
-                                    INSERTDATA(MAINCONNECTIONAPI,"CONNECTION",HEADER,DATA,(Datata)=>{
-                                        INSERTDATA(localStorage.getItem("API"),localStorage.getItem("Store"),HEADER,DATA,(Datatata)=>{
-                                            LOCALSTORE("User",Datata.uniqueId);
-                                            RELOAD();
-                                        });
+                                    INSERTDATA(MAIN,"User",HEADER,DATA,(Datata)=>{
+                                        LOCALSTORE("User",Datata.uniqueId);
+                                        RELOAD();
                                     });
                                 });
                             });
