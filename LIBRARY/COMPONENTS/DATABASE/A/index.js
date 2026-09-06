696d67b829048e0d0b3e43ef9bde1b209b682e42
deuelion <316406166+deuelion@users.noreply.github.com>
2026-09-06 12:53:10 +0300

    Updates

--- LIBRARY/COMPONENTS/DATABASE/A/index.js
+++ LIBRARY/COMPONENTS/DATABASE/A/index.js
@@ -2,6 +2,7 @@ export const ANCHOR=(HOLDER,SOURCE,CALLBACk)=>{
     ELEMENTED(HOLDER,"a",(DATA)=>{
         TEXTCOMPOMODE(DATA);
         WIDTH(DATA,"90%");
+        HEIGHT(DATA,"45px");
         SOURCEREF=(DATA,SOURCE);
         CALLBACk(DATA);
     });
