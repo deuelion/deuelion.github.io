835c65cbbb37f62bb69264ed3892e08082ef6f40
deuelion <316406166+deuelion@users.noreply.github.com>
2026-09-06 12:58:15 +0300

    Updates

--- LIBRARY/COMPONENTS/DATABASE/VIDEO/index.js
+++ LIBRARY/COMPONENTS/DATABASE/VIDEO/index.js
@@ -1,3 +1,6 @@
-export const VIDEO=()=>{
-
+export const VIDEO=(HOLDER,SOURCE,CALLBACk)=>{
+    ELEMENTED(HOLDER,"video",(DATA)=>{
+        SOURCED(DATA,SOURCE);
+        CALLBACk(DATA);
+    });
 };
